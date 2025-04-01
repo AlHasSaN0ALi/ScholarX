6705abcbcfcc39af88ac492eb137e774ae29144f
kimosoft1 <kariiimashraf.1@gmail.com>
2025-04-01 16:43:44 +0200

    fix: imports issue resolved

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,9 +3,9 @@ import NavBar from '../../components/NavBar/NavBar';
 // import Footer from '../../components/Footer/Footer';
 import Hero from './components/Hero/Hero';
 import Features from './components/Features/Features';
-import Services from './components/Services/Services';
-import Impact from './components/Impact/Impact';
-import Contact from './components/Contact/Contact';
+// import Services from './components/Services/Services';
+// import Impact from './components/Impact/Impact';
+// import Contact from './components/Contact/Contact';
 import './Home.css';
 
 function Home() {
