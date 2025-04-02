ca6b915709ebe47bb8b97e74846d5b1285eaf883
kimosoft1 <kariiimashraf.1@gmail.com>
2025-04-02 06:10:14 +0200

    chore: update dependencies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
+import About from './pages/About/About'
+import Services from './pages/Services/Services'
+import Courses from './pages/Courses/Courses'
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="app">
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/courses" element={<Courses />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
