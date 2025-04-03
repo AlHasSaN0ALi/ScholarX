31303989eb2499a54ba367c3ab20eaa0610d9e9b
Ahmed-Taha-981 <122402269+Ahmed-Taha-981@users.noreply.github.com>
2025-04-03 17:46:59 +0200

    feat: add login and signup pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import Home from './pages/Home/Home'
 import About from './pages/About/About'
 import Services from './pages/Services/Services'
 import Courses from './pages/Courses/Courses'
+import Login from './pages/Login/Login'
+import Signup from './pages/Signup/Signup'
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -16,6 +18,8 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
           <Route path="/courses" element={<Courses />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </div>
     </Router>
