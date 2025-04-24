814bf36a6d57e8b3ab967821c04e488d86cdf976
abdelrahman-elsaady <abdelrahmanelnobby@gmail.com>
2025-04-24 22:59:03 +0200

    resolve merge conflict

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,8 @@ import Services from './pages/Services/Services'
 import Courses from './pages/Courses/Courses'
 import Login from './pages/Login/Login'
 import Signup from './pages/Signup/Signup'
+import Contact from './pages/Contact/Contact'
 import './App.css'
-import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
@@ -20,6 +20,7 @@ function App() {
           <Route path="/courses" element={<Courses />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
     </Router>
